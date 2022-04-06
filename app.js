4aa5656ce2f39181a09552fd7e2d9f5a81704fc9
s546652 <98067009+s546652@users.noreply.github.com>
2022-04-05 23:54:50 -0500

    images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var houseRouter = require('./routes/house');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/house',houseRouter);
 app.use('/addmods',addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
